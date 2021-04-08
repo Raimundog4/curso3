c1f073220d758c9f5b3129485be51befc1b02cb9
raimu <raimu@LAPTOP-2NKS05GS>
2021-04-08 16:17:05 -0300

    Importanto o java.util

--- curso3/src/curso3/Curso3.java
+++ curso3/src/curso3/Curso3.java
@@ -1,10 +1,22 @@
 package curso3;
 
+import java.util.Date;
+
 public class Curso3 {
 
 	private String Nome;
 	
 	private String sobrenome;
+	
+	private Date dataNascimento;
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 
 	public String getNome() {
 		return Nome;
