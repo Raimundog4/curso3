21df4f744ca06e09a4becd9646810ac90dad0018
raimu <raimu@LAPTOP-2NKS05GS>
2021-04-08 16:15:13 -0300

    Criação do atributo Data

--- curso3/src/curso3/Curso3.java
+++ curso3/src/curso3/Curso3.java
@@ -2,4 +2,24 @@ package curso3;
 
 public class Curso3 {
 
+	private String Nome;
+	
+	private String sobrenome;
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
 }
