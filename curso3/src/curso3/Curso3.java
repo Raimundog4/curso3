b635dbb7607e8ba54f912916ced955d29692e2d8
raimu <raimu@LAPTOP-2NKS05GS>
2021-04-09 14:40:51 -0300

    Revert "Adicionando date2"
    This reverts commit 2aa9ef123e924dd643fb09a0cf2ffa541bb20f6a.

--- curso3/src/curso3/Curso3.java
+++ curso3/src/curso3/Curso3.java
@@ -9,16 +9,6 @@ public class Curso3 {
 	private String sobrenome;
 	
 	private Date dataNascimento;
-	
-	private Date dataNascimento2;
-
-	public Date getDataNascimento2() {
-		return dataNascimento2;
-	}
-
-	public void setDataNascimento2(Date dataNascimento2) {
-		this.dataNascimento2 = dataNascimento2;
-	}
 
 	public Date getDataNascimento() {
 		return dataNascimento;
